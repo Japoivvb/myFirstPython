25b073de5c5952633591ec68995d44799be6288a
Japoivvb <jose.portugal@stucom.com>
2026-04-17 14:36:45 +0200

    classes

---
+++ 09_POO/90_Lab_Vehicles.py
@@ -0,0 +1,30 @@
+class Vehicle:
+    def __init__(self, mark="",  speed=0):
+        self.mark = mark
+        self.speed = speed
+    
+    def accelerate(self, speed):
+        self.speed += speed
+        
+    def stats(self):
+        print(self.mark, self.speed )
+
+class Car(Vehicle):
+    def hornet(self):
+        print("piii")
+        
+class Bicyle(Vehicle):
+    def step(self):
+        print("bike is moving")
+
+v1 = Vehicle("Seat", 120)
+v1.accelerate(15)
+v1.stats()
+
+c1 = Car("BMW",100)
+c1.accelerate(50)
+c1.stats()
+
+b1 = Bicyle("BH", 10)
+b1.accelerate(15)
+b1.stats()
